b6ef1e85f5182e285ba2c491475f3f32ec64ffa1
Umesh Shete <umesh.shete2@mastercard.com>
2019-04-15 18:43:20 +0530

    First Commit

---
+++ McomNamingServer/src/test/java/com/us/microservice/McomNamingServer/McomNamingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.us.microservice.McomNamingServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class McomNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
