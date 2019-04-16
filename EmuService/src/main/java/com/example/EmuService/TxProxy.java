7e9ff113caaae756ead0319d8dddbb4aa0960b1e
Umesh Shete <umesh.shete2@mastercard.com>
2019-04-16 18:33:43 +0530

    Added Emul service - With Feign proxy-version 2(Ribbon-with hardcoded urls)

--- EmuService/src/main/java/com/example/EmuService/TxProxy.java
+++ EmuService/src/main/java/com/example/EmuService/TxProxy.java
@@ -1,10 +1,13 @@
 package com.example.EmuService;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="DwhService", url="http://localhost:8091")
+//@FeignClient(name="DwhService", url="http://localhost:8091")
+@FeignClient(name="DwhService")
+@RibbonClient(name="DwhService")
 public interface TxProxy {
 	@GetMapping("/txs/{p}/{arnum}")
 	public TxDetail getTxDetails(@PathVariable(name="p") String p, @PathVariable(name="arnum") String arnum);
